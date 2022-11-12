d2ed44a7587f20a4cb80248aa5842de74468c6c5
Zhang Jiexi <62730172+Zhang-Jiexi@users.noreply.github.com>
2022-11-12 14:53:01 +0800

    main file

---
+++ setup/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setuptools.setup(
+  name="DataProTools",
+  version="1.0.0",
+  author="Zhang Jiexi",
+  author_email="zhangjiexi66696@outlook.com",
+  description="",
+  long_description=long_description,
+  long_description_content_type="text/markdown",
+  url="https://github.com/pypa/sampleproject",
+  packages=setuptools.find_packages(),
+  classifiers=[
+  "Programming Language :: Python :: 3",
+  "License :: OSI Approved :: MIT License",
+  "Operating System :: OS Independent",
+  ],
+)
